9f93a801ca0a9879309a3a6e4005162bae06c30e
HerinOd <hieudo851@gmail.com>
2025-05-18 17:16:24 +0700

    hi

--- src/com/henrizz/Main.java
+++ src/com/henrizz/Main.java
@@ -6,19 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
         String story = "Bluvia, who sets out in revenge for Radley's murder." +
-                "\nMeanwhile, Judo, a soldier who becomes involved in a conflict between her militia and a religious cult.";
+                "\nMeanwhile, Judoni, a soldier who becomes involved in a conflict between her militia and a religious cult.";
         System.out.println(story);
         byte x = 18;
         short time = (short)(x + 12);
-        String searchFor = "Judo";
+        String searchFor = "Judoni";
         String destination = "Seatle";
         String[] coordinate = { "1890", "87", "8670" };
         String[][] info = { { "Bluvia", "Macky", "Digger", "Maxine" }
                                      ,{ "21", "21", "23", "19" } };
-        System.out.println("Enemy: " + searchFor);
+        System.out.println("Find: " + searchFor);
         System.out.println("Go to: " + destination);
         System.out.println("Location: " + Arrays.toString(coordinate));
         System.out.println("Last: " + time + " days.");
-        System.out.println("Members: " + Arrays.deepToString(info));
+        System.out.println("Members and Ages: " + Arrays.deepToString(info));
      }
 }
