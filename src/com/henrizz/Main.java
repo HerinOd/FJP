442d362a8e2f31ecb6425acfecb00eeaf22a3cff
HerinOd <hieudo851@gmail.com>
2025-05-24 00:43:56 +0700

    Candidates Evaluation System Project #1

--- src/com/henrizz/Main.java
+++ src/com/henrizz/Main.java
@@ -1,37 +1,48 @@
 package com.henrizz;
 
-import java.text.NumberFormat;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-        System.out.print("Your Principal: ");
-        float principal = input.nextFloat();
-        System.out.print("The annual interest rate: ");
-        float annualinterest1 = input.nextFloat();
-        System.out.print("Your Period (Years): ");
-        byte period = input.nextByte();
+        //Candidates Evaluation System Project:
 
-        float annualinterest2 = annualinterest1 / 100;
-        float monthlyinterest = annualinterest2 / 12;
-        double mortgage0 = Math.pow(1 + monthlyinterest, period * 12);
-        double mortgage1 = monthlyinterest * mortgage0;
-        double mortgage2 = mortgage0 - 1;
-        double mortgage3 = principal * (mortgage1 / mortgage2);
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String mortgage = currency.format(mortgage3);
-        NumberFormat percent = NumberFormat.getPercentInstance();
-        String annualinterest = percent.format(annualinterest2);
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("====== MORTGAGE CALCULATOR ======");
-        System.out.println("Principal: " + principal);
-        System.out.println("Annual Interest Rate: " + annualinterest);
-        System.out.println("Period (Years): " + period);
-        System.out.println("Mortgage: " + mortgage);
-        System.out.println("=================================");
+        System.out.print("Your name: ");
+        String name = input.nextLine();
+        System.out.print("Your age: ");
+        byte age = input.nextByte();
+        System.out.print("Your experience (Years): ");
+        byte experience = input.nextByte();
+        System.out.print("Your GPA: ");
+        double gpa = input.nextDouble();
+        System.out.print("Do you have a 6.5 Ielts score? (true/false): ");
+        boolean ieltsScore = input.nextBoolean();
+        System.out.print("Do you have any criminal records? (true/false): ");
+        boolean criminalRecords = input.nextBoolean();
 
-     }
+        boolean isEligible = (age >= 22)
+                         && ((experience >= 2) || (gpa >= 3.2))
+                         && (ieltsScore == true)
+                         && (criminalRecords == false);
+
+        String classification;
+        if ((isEligible == true) && (gpa >= 3.8))
+            classification = "Excellent candidate";
+        else if ((isEligible == true) && (gpa >= 3.2))
+            classification = "Potential candidate";
+        else if (isEligible == true)
+            classification = "Average candidate";
+        else
+            classification = "Denied";
+
+        System.out.println("====== YOUR RESULT ======");
+        System.out.println("Name: " + name);
+        System.out.println("Eligible for interview: " + isEligible);
+        System.out.println("Classification: " + classification);
+
+
+
+    }
 }
