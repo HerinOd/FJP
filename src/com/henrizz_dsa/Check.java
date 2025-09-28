1c2872cb94de6e1fe01d6ac1b14e32364c8d8fc1
HerinOd <hieudo851@gmail.com>
2025-09-28 21:48:30 +0700

    28/09/25

--- src/com/henrizz_dsa/Check.java
+++ src/com/henrizz_dsa/Check.java
@@ -2,6 +2,6 @@ package com.henrizz_dsa;
 //*FIXED*
 public class Check {
     public static void main(String[] args) {
-        System.out.println("Hello, World! It's DSA time!");
+        System.out.println("Hello, World!");
     }
 }
